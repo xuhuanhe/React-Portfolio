276c80c5b2ac2da956501d2073dd43f418d2f6ae
Xuhuan He <xuhuanhe16@example.com>
2022-06-20 23:14:33 -0400

    Added comment on AnimatedLetters scss

--- src/components/AnimatedLetters/index.js
+++ src/components/AnimatedLetters/index.js
@@ -4,6 +4,7 @@ const AnimatedLetters = ({ letterClass, strArray, idx }) => {
   return (
     <span>
       {strArray.map((char, i) => (
+        //char + i because we may have repeated letters
         <span key={char + i} className={`${letterClass} _${i + idx}`}>
           {char}
         </span>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+    
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['u', 'h', 'u', 'a', 'n', ' ', 'H', 'e']
     const jobArray = ['w', 'e', 'b',' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
@@ -31,7 +32,7 @@ const Home = () => {
                 <br />
                 <AnimatedLetters letterClass={letterClass}
                 strArray={jobArray}
-                idx={22} />
+                idx={23} />
                 </h1>
                 <h2>Frontend developer / Backen developer</h2>
                 <Link to="contact" className='flat-button'>CONTACT ME</Link>
