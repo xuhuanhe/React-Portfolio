5431085bd754f20c1892bff89741de60fc5a307d
Xuhuan He <xuhuanhe16@example.com>
2022-06-28 18:59:55 -0400

    Draw SVG Logo animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
-
+import Logo from './Logo'
 const Home = () => {
     
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -35,8 +35,11 @@ const Home = () => {
                 idx={23} />
                 </h1>
                 <h2>Frontend developer / Backen developer</h2>
-                <Link to="contact" className='flat-button'>CONTACT ME</Link>
+                <Link to="contact" className='flat-button'>
+                    CONTACT ME
+                </Link>
             </div>
+            <Logo />
         </div>
     )
 }
